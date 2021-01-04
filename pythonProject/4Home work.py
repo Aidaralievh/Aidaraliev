256506ba285f71debd798dbc67cefe261462ae98
Aidaralievh <a.aidaraliev26@gmail.com>
2021-01-04 23:48:19 +0600

    commit

--- pythonProject/4Home work.py
+++ pythonProject/4Home work.py
@@ -1,9 +1,9 @@
 from random import randint
 
-a=randint(0,9)
-b=randint(0,9)
-c=randint(0,9)
-d=randint(0,9)
+a = randint(0, 9)
+b = randint(0, 9)
+c = randint(0, 9)
+d = randint(0,9)
 st0 = str(a) + str(b) + str(c) + str(d)
 
 
@@ -17,14 +17,14 @@ while True:
         print(st0)
         continue
     if len(st) != 4:
-        print ("должно быть 4 цифры")
+        print("должно быть 4 цифры")
         continue
-    Is=list(st)
+    Is = list(st)
     Is0 = list(st0)
 
     bulls = 0
     for j in range(4):
-        if Is [j] == Is0[j]:
+        if Is[j] == Is0[j]:
             bulls += 1
             Is[j] = ("")
             Is0[j] = ("*")
